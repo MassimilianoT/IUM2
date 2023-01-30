3b5f730d1531be6762862e1a9df89b934ca1a973
Mirko Glisenti <mirko.glisenti1999@gmail.com>
2023-01-30 13:01:25 +0100

    ...

--- resources/views/admin/boardgames/create.blade.php
+++ resources/views/admin/boardgames/create.blade.php
@@ -6,9 +6,9 @@
 
             <x-form.input name="name" label="Nome"/>
 
-            <x-form.input label="Numero di giocatori minimi" name="minPlayers" type="number" min=1 />
+            <x-form.input label="Numero di giocatori minimi" name="minPlayers" type="number" min=1/>
 
-            <x-form.input label="Numero di giocatori massimi" name="maxPlayers" type="number" min=1 />
+            <x-form.input label="Numero di giocatori massimi" name="maxPlayers" type="number" min=1/>
 
             <x-form.input label="Editore" name="editor"/>
 
@@ -19,12 +19,15 @@
 
                 <div id="authorsList">
                     @foreach ($authors as $author)
-                        <input type="checkbox" name="authors_id[]" value={{ $author->id }} id="author_{{ $author->id }}">
-                        <label for="author_{{ $author->id }}">{{ ucwords($author->firstName . " ". $author->lastName) }}</label><br>
+                        <input type="checkbox" name="authors_id[]"
+                               value={{ $author->id }} id="author_{{ $author->id }}">
+                        <label for="author_{{ $author->id }}">{{ ucwords($author->firstName . " ". $author->lastName) }}</label>
+                        <br>
                     @endforeach
                 </div>
 
-                <button class="mt-2 block text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-0 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="author-modal">
+                <button class="mt-2 block text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-0 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        type="button" data-modal-toggle="author-modal">
                     +
                 </button>
 
@@ -36,12 +39,14 @@
 
                 <div id="categoriesList">
                     @foreach ($categories as $category)
-                        <input type="checkbox" name="categories_id[]" value={{ $category->id }} id="category_{{ $category->id }}">
+                        <input type="checkbox" name="categories_id[]"
+                               value={{ $category->id }} id="category_{{ $category->id }}">
                         <label for="category_{{ $category->id }}">{{ ucwords($category->name) }}</label><br>
                     @endforeach
                 </div>
 
-                <button class="mt-2 block text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-0 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="category-modal">
+                <button class="mt-2 block text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-0 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        type="button" data-modal-toggle="category-modal">
                     +
                 </button>
 
@@ -54,12 +59,19 @@
         </form>
     </x-admin-setting>
 
-    <div id="author-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+    <div id="author-modal" tabindex="-1" aria-hidden="true"
+         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
         <div class="relative p-4 w-full max-w-md h-full md:h-auto">
             <!-- Modal content -->
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <button id="closeAuthorModal" type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="author-modal">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <button id="closeAuthorModal" type="button"
+                        class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                        data-modal-toggle="author-modal">
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                              clip-rule="evenodd"></path>
+                    </svg>
                 </button>
                 <div class="py-6 px-6 lg:px-8">
                     <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Crea nuovo autore</h3>
@@ -70,20 +82,36 @@
 
                         <x-form.input name="lastName" label="Cognome"/>
 
-                        <x-form.button id="createAuthor">Crea</x-form.button>
-
+                        <x-form.field>
+                            <button type="submit"
+                                    class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600"
+                                    id="createAuthor">
+                                Crea
+                            </button>
+                            <button data-modal-toggle="author-modal" type="button"
+                                    class="bg-gray-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-gray-600 mx-auto">
+                                Chiudi
+                            </button>
+                        </x-form.field>
                     </form>
                 </div>
             </div>
         </div>
     </div>
 
-    <div id="category-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+    <div id="category-modal" tabindex="-1" aria-hidden="true"
+         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
         <div class="relative p-4 w-full max-w-md h-full md:h-auto">
             <!-- Modal content -->
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <button id="closeCategoryModal" type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="category-modal">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <button id="closeCategoryModal" type="button"
+                        class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                        data-modal-toggle="category-modal">
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                              clip-rule="evenodd"></path>
+                    </svg>
                 </button>
                 <div class="py-6 px-6 lg:px-8">
                     <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Crea nuova categoria</h3>
@@ -94,7 +122,17 @@
 
                         <x-form.input name="slug" label="Slug"/>
 
-                        <x-form.button id="createCategory">Crea</x-form.button>
+                        <x-form.field>
+                            <button type="submit"
+                                    class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600"
+                                    id="createCategory">
+                                Crea
+                            </button>
+                            <button data-modal-toggle="category-modal" type="button"
+                                    class="bg-gray-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-gray-600 mx-auto">
+                                Chiudi
+                            </button>
+                        </x-form.field>
                     </form>
                 </div>
             </div>
@@ -103,16 +141,16 @@
 </x-layout>
 
 <script type="text/javascript">
-    $( document ).ready(function() {
-        $("#minPlayers").on("input", function(){
+    $(document).ready(function () {
+        $("#minPlayers").on("input", function () {
             $('#maxPlayers').attr('min', $(this).val());
         });
 
-        $("#categoryName").on("input", function(){
-            $('#slug').val($(this).val().toLowerCase().replace(/ /g,'-'));
+        $("#categoryName").on("input", function () {
+            $('#slug').val($(this).val().toLowerCase().replace(/ /g, '-'));
         });
 
-        $('#createCategory').click(function(e){
+        $('#createCategory').click(function (e) {
 
             e.preventDefault();
 
@@ -128,8 +166,8 @@
             $.ajax({
                 type: "POST",
                 url: '/admin/categories/inline',
-                data: { _token: CSRF_TOKEN, name:name, slug:slug },
-                success: function(response){
+                data: {_token: CSRF_TOKEN, name: name, slug: slug},
+                success: function (response) {
                     $('#categoryName').val('');
                     $('#slug').val('');
                     $('#closeCategoryModal').click();
@@ -138,7 +176,7 @@
             })
         });
 
-        $('#createAuthor').click(function(e){
+        $('#createAuthor').click(function (e) {
 
             e.preventDefault();
 
@@ -154,8 +192,8 @@
             $.ajax({
                 type: "POST",
                 url: '/admin/authors/inline',
-                data: { _token: CSRF_TOKEN, firstName:firstName, lastName:lastName },
-                success: function(response){
+                data: {_token: CSRF_TOKEN, firstName: firstName, lastName: lastName},
+                success: function (response) {
                     $('#firstName').val('');
                     $('#lastName').val('');
                     $('#closeAuthorModal').click();
--- resources/views/components/admin-setting.blade.php
+++ resources/views/components/admin-setting.blade.php
@@ -9,34 +9,42 @@
         <aside class="w-48 flex-shrink-0">
             <ul>
                 <li>
-                    <a href="/admin/boardgames/create" class="{{ request()->is('admin/boardgames/create') ? 'text-blue-500' : ''}}">Aggiungi Gioco</a>
+                    <a href="/admin/boardgames/create"
+                       class="{{ request()->is('admin/boardgames/create') ? 'text-blue-500' : ''}}">Aggiungi Gioco</a>
                 </li>
                 <li>
-                    <a href="/admin/boardgames" class="{{ request()->is('admin/boardgames') ? 'text-blue-500' : ''}}">Gestisci Giochi</a>
+                    <a href="/admin/boardgames" class="{{ request()->is('admin/boardgames') ? 'text-blue-500' : ''}}">Gestisci
+                        Giochi</a>
                 </li>
                 <hr class="w-36 h-1 mx-auto my-2 bg-gray-100 border-0 rounded dark:bg-gray-700">
 
                 <li>
-                    <a href="/admin/authors/create" class="{{ request()->is('admin/authors/create') ? 'text-blue-500' : ''}}">Aggiungi Autore</a>
+                    <a href="/admin/authors/create"
+                       class="{{ request()->is('admin/authors/create') ? 'text-blue-500' : ''}}">Aggiungi Autore</a>
                 </li>
-                                <li>
-                    <a href="/admin/authors" class="{{ request()->is('admin/authors') ? 'text-blue-500' : ''}}">Gestisci Autori</a>
+                <li>
+                    <a href="/admin/authors" class="{{ request()->is('admin/authors') ? 'text-blue-500' : ''}}">Gestisci
+                        Autori</a>
+                </li>
+                <hr class="w-36 h-1 mx-auto my-2 bg-gray-100 border-0 rounded dark:bg-gray-700">
+                <li>
+                    <a href="/admin/categories/create"
+                       class="{{ request()->is('admin/categories/create') ? 'text-blue-500' : ''}}">Aggiungi
+                        Categoria</a>
+                </li>
+                <li>
+                    <a href="/admin/categories" class="{{ request()->is('admin/categories') ? 'text-blue-500' : ''}}">Gestisci
+                        Categorie</a>
                 </li>
                 <hr class="w-36 h-1 mx-auto my-2 bg-gray-100 border-0 rounded dark:bg-gray-700">
 
                 <li>
-                    <a href="/admin/categories" class="{{ request()->is('admin/categories') ? 'text-blue-500' : ''}}">Gestisci Categorie</a>
+                    <a href="/admin/users" class="{{ request()->is('admin/users') ? 'text-blue-500' : ''}}">Gestisci
+                        Utenti</a>
                 </li>
                 <li>
-                    <a href="/admin/categories/create" class="{{ request()->is('admin/categories/create') ? 'text-blue-500' : ''}}">Aggiungi Categoria</a>
-                </li>
-                <hr class="w-36 h-1 mx-auto my-2 bg-gray-100 border-0 rounded dark:bg-gray-700">
-
-                <li>
-                    <a href="/admin/users" class="{{ request()->is('admin/users') ? 'text-blue-500' : ''}}">Gestisci Utenti</a>
-                </li>
-                <li>
-                    <a href="/admin/plays" class="{{ request()->is('admin/plays') ? 'text-blue-500' : ''}}">Gestisci Partite</a>
+                    <a href="/admin/plays" class="{{ request()->is('admin/plays') ? 'text-blue-500' : ''}}">Gestisci
+                        Partite</a>
                 </li>
             </ul>
         </aside>
